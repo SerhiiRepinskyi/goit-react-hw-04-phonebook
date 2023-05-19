aa6f90d841479a5c42111d3098e1093e5dacfa4d
Serhii Repinskyi <115186821+serhiirepinskyi@users.noreply.github.com>
2023-05-20 00:48:25 +0300

    update ContactForm with hooks

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Form, FormLabel, FormInput, FormBtn } from './ContactForm.styled';
 
@@ -7,70 +7,64 @@ const INITIAL_STATE = {
   number: '',
 };
 
-class ContactForm extends Component {
-  state = { ...INITIAL_STATE };
+export default function ContactForm({ onSubmit }) {
+  const [formData, setFormData] = useState({ ...INITIAL_STATE });
 
   // Обробник для всіх інпутів, інпути розрізняються за атрибутом name
   // Відповідає за оновлення стану
-  handleInputChange = evt => {
+  const handleInputChange = evt => {
     // console.log(evt.currentTarget.name);
     // console.log(evt.currentTarget.value);
     const { name, value } = evt.currentTarget;
-    this.setState({ [name]: value });
+    setFormData(prevData => ({ ...prevData, [name]: value }));
   };
 
   // Викликається під час відправлення форми
-  handleSubmit = evt => {
+  const handleSubmit = evt => {
     evt.preventDefault();
-    // const { name, number } = this.state;
-    // console.log(`Name: ${name}, Number: ${number}`);
-    this.props.onSubmit({ ...this.state });
-    this.resetForm();
+    onSubmit(formData);
+    resetForm();
   };
 
-  resetForm = () => {
-    this.setState({ ...INITIAL_STATE });
+  const resetForm = () => {
+    setFormData({ ...INITIAL_STATE });
   };
 
-  render() {
-    const { name, number } = this.state;
+  const { name, number } = formData;
 
-    return (
-      <Form onSubmit={this.handleSubmit}>
-        <FormLabel>
-          Name
-          <FormInput
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            value={name}
-            onChange={this.handleInputChange}
-          />
-        </FormLabel>
+  return (
+    <Form onSubmit={handleSubmit}>
+      <FormLabel>
+        Name
+        <FormInput
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          value={name}
+          onChange={handleInputChange}
+        />
+      </FormLabel>
 
-        <FormLabel>
-          Number
-          <FormInput
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            value={number}
-            onChange={this.handleInputChange}
-          />
-        </FormLabel>
+      <FormLabel>
+        Number
+        <FormInput
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          value={number}
+          onChange={handleInputChange}
+        />
+      </FormLabel>
 
-        <FormBtn type="submit">Add contact</FormBtn>
-      </Form>
-    );
-  }
+      <FormBtn type="submit">Add contact</FormBtn>
+    </Form>
+  );
 }
 
 ContactForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
 };
-
-export default ContactForm;

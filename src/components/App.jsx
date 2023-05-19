eda8fe39a2054204f8fe03074f57a1ef5a42babe
Serhii Repinskyi <115186821+serhiirepinskyi@users.noreply.github.com>
2023-05-19 23:46:45 +0300

    update App with hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import ContactForm from './ContactForm';
 import ContactList from './ContactList';
@@ -12,37 +12,43 @@ import {
   Message,
 } from './App.styled';
 
-class App extends Component {
-  state = {
-    contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+export default function App() {
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem('contacts')) ?? []
+  );
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
-    // console.log('App componentDidMount');
-    const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
+  // contacts: [
+  //   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  //   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  //   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  //   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  // ];
 
-  componentDidUpdate(prevProps, prevState) {
-    // console.log('App componentDidUpdate');
-    if (this.state.contacts !== prevState.contacts) {
-      // console.log('Оновилося поле contacts');
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
+  // componentDidMount() {
+  //   // console.log('App componentDidMount');
+  //   const contacts = localStorage.getItem('contacts');
+  //   const parsedContacts = JSON.parse(contacts);
 
-  addContact = ({ name, number }) => {
-    const isAdded = this.state.contacts.find(
+  //   if (parsedContacts) {
+  //     this.setState({ contacts: parsedContacts });
+  //   }
+  // }
+
+  // componentDidUpdate(prevProps, prevState) {
+  //   // console.log('App componentDidUpdate');
+  //   if (this.state.contacts !== prevState.contacts) {
+  //     // console.log('Оновилося поле contacts');
+  //     localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  //   }
+  // }
+
+  const addContact = ({ name, number }) => {
+    const isAdded = contacts.find(
       contact => contact.name.toLowerCase() === name.toLowerCase()
     );
     if (isAdded) {
@@ -54,13 +60,11 @@ class App extends Component {
       name: name,
       number: number,
     };
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, newContact],
-    }));
+
+    setContacts(prevContacts => [...prevContacts, newContact]);
   };
 
-  getVisibleContacts = () => {
-    const { filter, contacts } = this.state;
+  const getVisibleContacts = () => {
     const normalizedFilter = filter.toLowerCase();
 
     return contacts.filter(contact =>
@@ -68,46 +72,40 @@ class App extends Component {
     );
   };
 
-  changeFilter = evt => {
-    const { value } = evt.currentTarget;
-    this.setState({ filter: value });
+  const changeFilter = evt => {
+    setFilter(evt.currentTarget.value.trim());
   };
 
-  deleteContact = todoId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== todoId),
-    }));
-  };
-
-  render() {
-    const { contacts, filter } = this.state;
-    const visibleContacts = this.getVisibleContacts();
-
-    return (
-      <Container>
-        <Title>Phonebook</Title>
-        <ContactForm onSubmit={this.addContact} />
-
-        <Subtitle>Contacts</Subtitle>
-        <AmountContacts>
-          All contacts in the phonebook:{' '}
-          <ContactsNum>{contacts.length}</ContactsNum>
-        </AmountContacts>
-
-        {contacts.length > 0 ? (
-          <>
-            <Filter value={filter} onChange={this.changeFilter} />
-            <ContactList
-              contacts={visibleContacts}
-              deleteContact={this.deleteContact}
-            />
-          </>
-        ) : (
-          <Message>Contact list is empty</Message>
-        )}
-      </Container>
+  const deleteContact = todoId => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== todoId)
     );
-  }
-}
+  };
 
-export default App;
+  const visibleContacts = getVisibleContacts();
+
+  return (
+    <Container>
+      <Title>Phonebook</Title>
+      <ContactForm onSubmit={addContact} />
+
+      <Subtitle>Contacts</Subtitle>
+      <AmountContacts>
+        All contacts in the phonebook:{' '}
+        <ContactsNum>{contacts.length}</ContactsNum>
+      </AmountContacts>
+
+      {contacts.length > 0 ? (
+        <>
+          <Filter value={filter} onChange={changeFilter} />
+          <ContactList
+            contacts={visibleContacts}
+            deleteContact={deleteContact}
+          />
+        </>
+      ) : (
+        <Message>Contact list is empty</Message>
+      )}
+    </Container>
+  );
+}
